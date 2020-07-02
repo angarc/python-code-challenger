3c42481c8d0aa3970f31ec2349a89e2959df4ef5
Garcia <garcia@Garcias-MacBook-Pro.local>
2020-07-01 21:37:58 -0600

    Updates

---
+++ precache-manifest.93926eaf0659cac78cd97127be4b28e1.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b197d61e8ce1e01051423916be9cd56e",
+    "url": "/python-code-challenger/index.html"
+  },
+  {
+    "revision": "f0349f3b76a883ee69e5",
+    "url": "/python-code-challenger/static/css/2.d09ab2fc.chunk.css"
+  },
+  {
+    "revision": "e728ffd51aba31a8fdeb",
+    "url": "/python-code-challenger/static/css/main.b47e0462.chunk.css"
+  },
+  {
+    "revision": "f0349f3b76a883ee69e5",
+    "url": "/python-code-challenger/static/js/2.d4de690a.chunk.js"
+  },
+  {
+    "revision": "a398cfb19cd4fd6bd9d0522fe508fcdf",
+    "url": "/python-code-challenger/static/js/2.d4de690a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e728ffd51aba31a8fdeb",
+    "url": "/python-code-challenger/static/js/main.dc02a99c.chunk.js"
+  },
+  {
+    "revision": "a6f8dabd38c30306c95e",
+    "url": "/python-code-challenger/static/js/runtime-main.8e297237.js"
+  },
+  {
+    "revision": "cb04446b5093309a054ccbfc2289238c",
+    "url": "/python-code-challenger/static/media/bear_1.cb04446b.jpeg"
+  },
+  {
+    "revision": "f29b4fd12abce78923d12ed7cf69da6b",
+    "url": "/python-code-challenger/static/media/bear_2.f29b4fd1.JPG"
+  },
+  {
+    "revision": "4a5d4d855d11fae79cc864bdacb2f479",
+    "url": "/python-code-challenger/static/media/fa-brands-400.4a5d4d85.eot"
+  },
+  {
+    "revision": "4c1da237bdae0773309df93b2cd48e09",
+    "url": "/python-code-challenger/static/media/fa-brands-400.4c1da237.ttf"
+  },
+  {
+    "revision": "5734d789b25228cbafc64a58ae971aca",
+    "url": "/python-code-challenger/static/media/fa-brands-400.5734d789.woff"
+  },
+  {
+    "revision": "778b1f251bea7412048da95b87bf816f",
+    "url": "/python-code-challenger/static/media/fa-brands-400.778b1f25.svg"
+  },
+  {
+    "revision": "91a23e8bf2b4b84c39311cb5eb23aaa0",
+    "url": "/python-code-challenger/static/media/fa-brands-400.91a23e8b.woff2"
+  },
+  {
+    "revision": "260be4f29c0b2ce47480afb23f38f237",
+    "url": "/python-code-challenger/static/media/fa-regular-400.260be4f2.ttf"
+  },
+  {
+    "revision": "5c674c9216c06ede2f618aa58ae71116",
+    "url": "/python-code-challenger/static/media/fa-regular-400.5c674c92.woff2"
+  },
+  {
+    "revision": "66578cdbb6dc01f527a53971051b3e85",
+    "url": "/python-code-challenger/static/media/fa-regular-400.66578cdb.svg"
+  },
+  {
+    "revision": "6b20949b3a679c30d09f64acd5d3317d",
+    "url": "/python-code-challenger/static/media/fa-regular-400.6b20949b.eot"
+  },
+  {
+    "revision": "d44ad00c44e46fd29f6126fa7d888cde",
+    "url": "/python-code-challenger/static/media/fa-regular-400.d44ad00c.woff"
+  },
+  {
+    "revision": "412a43d6840addd683665ec12c30f810",
+    "url": "/python-code-challenger/static/media/fa-solid-900.412a43d6.woff2"
+  },
+  {
+    "revision": "486853107489520b3265b19b191626f8",
+    "url": "/python-code-challenger/static/media/fa-solid-900.48685310.svg"
+  },
+  {
+    "revision": "9a1672a8a8d91fbf82c71f451d495253",
+    "url": "/python-code-challenger/static/media/fa-solid-900.9a1672a8.eot"
+  },
+  {
+    "revision": "c65d154888aa166982dac3e72e7380ec",
+    "url": "/python-code-challenger/static/media/fa-solid-900.c65d1548.ttf"
+  },
+  {
+    "revision": "f3a7d3b5880544a91e9a7e6f8f35d4d2",
+    "url": "/python-code-challenger/static/media/fa-solid-900.f3a7d3b5.woff"
+  },
+  {
+    "revision": "27478925398720927de27a12bcce06ec",
+    "url": "/python-code-challenger/static/media/obi_1.27478925.png"
+  },
+  {
+    "revision": "3e90fb4c8a8a4083d7abc01ffd67e251",
+    "url": "/python-code-challenger/static/media/obi_2.3e90fb4c.jpeg"
+  },
+  {
+    "revision": "c042a11aea4b5d5ef7e8edba62bf9e49",
+    "url": "/python-code-challenger/static/media/yellow-rabbit.c042a11a.otf"
+  }
+]);
